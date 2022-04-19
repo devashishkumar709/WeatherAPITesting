f0f054b54f4fe702cc0632947d9706ca5f3aeb1b
devashishkumar709 <57445475+devashishkumar709@users.noreply.github.com>
2022-04-19 10:11:19 +0530

    Weather API added

---
+++ entry/src/main/js/default/pages/index/index.js
@@ -0,0 +1,34 @@
+export default {
+    data: {
+    },
+    weather_api(e){
+        var data4 = JSON.stringify(item4);
+        this.weather_api_input = JSON.parse(data4);
+        //console.log(this.weather_api_input[0].api_key);
+        //const url =  "https://api.openweathermap.org/data/2.5/weather?lat="+this.weather_api_input[0].latitude+"&lon="+this.weather_api_input[0].longitude+"&appid="+this.weather_api_input[0].api_key;
+        //console.log(url);
+
+        let data;
+        fetch.fetch({
+            url :  "https://api.openweathermap.org/data/2.5/weather?lat="+this.weather_api_input[0].latitude+"&lon="+this.weather_api_input[0].longitude+"&appid="+this.weather_api_input[0].api_key,
+            responseType:"json",
+            method: 'GET',
+            success:function(resp)
+            {
+                data = JSON.parse(resp);
+            },
+            fail:(data,code) => {
+                console.log("fail data: "+ JSON.stringify(data) + " fail code: "+ code );
+            },
+            complete: ()=>{
+                this.weather = data.weather.main ;
+                console.log("Weather :"+data.weather.main)
+            }
+        })
+
+        //console.log(this.weather_api_output);
+
+        this.weather_icon = "http://openweathermap.org/img/w/"+"50n"+".png";
+
+    },
+}
